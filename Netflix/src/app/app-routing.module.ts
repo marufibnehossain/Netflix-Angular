ce8bc52ad0120e57e182638a7d098bfd3ff7957f
Maruf <marufibnhossain@gmail.com>
2023-04-19 19:41:21 +0600

    fourth commit

--- Netflix/src/app/app-routing.module.ts
+++ Netflix/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './Components/home/home.component';
-import { SearchComponent } from './Components/search/search.component';
-import { MovieDetailsComponent } from './Components/movie-details/movie-details.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { SearchComponent } from './Pages/search/search.component';
+import { MovieDetailsComponent } from './Pages/movie-details/movie-details.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
--- Netflix/src/app/app.module.ts
+++ Netflix/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Components/home/home.component';
-import { SearchComponent } from './Components/search/search.component';
-import { MovieDetailsComponent } from './Components/movie-details/movie-details.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { SearchComponent } from './Pages/search/search.component';
+import { MovieDetailsComponent } from './Pages/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
